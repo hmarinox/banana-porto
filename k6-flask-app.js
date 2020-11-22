941c93e9eca0085228dfcb0a4dbb55fbf495e270
Humberto Marino Oliveira <humberto.marino@gmail.com>
2020-11-22 02:01:17 -0300

    ajuste k6

--- k6-flask-app.js
+++ k6-flask-app.js
@@ -3,5 +3,7 @@ import http from 'k6/http';
 	
 	export default function () {
 	  http.get('http://localhost:8000/visitor');
+	  http.get('http://localhost:8000/500');
+	  http.get('http://localhost:8000/404');
 	  sleep(1);
 	}
