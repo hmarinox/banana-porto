bb1d39a5bd40af6c2805aa2573e06bb715489ec8
Humberto Marino Oliveira <humberto.marino@gmail.com>
2020-11-22 12:17:28 -0300

    configuração k6 stress-test

--- k6-flask-app.js
+++ k6-flask-app.js
@@ -1,10 +1,14 @@
 import http from 'k6/http';
-	import { sleep } from 'k6';
-	
-	export default function () {
-	  http.get('http://localhost:8000/visitor');
-	  http.get('http://localhost:8000/500');
-	  http.get('http://localhost:8000/404');
-	  http.get('http://localhost:8000/divbyzero');
-	  sleep(1);
-	}
+import { sleep } from 'k6';
+
+export visitor function () {
+	http.get('http://localhost:8000/visitor');
+	sleep(1);
+}
+export http500 function () {
+	http.get('http://localhost:8000/visitor');
+	http.get('http://localhost:8000/500');
+	http.get('http://localhost:8000/404');
+	http.get('http://localhost:8000/divbyzero');
+	sleep(1);
+}
